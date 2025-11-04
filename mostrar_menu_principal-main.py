a3985029fcea50e71da353bb1ce42702d3c9cb83
nico-7483 <nico.riquelme009@gmail.com>
2025-11-04 00:21:45 -0300

    Delete mostrar_menu_principal-main.py

--- mostrar_menu_principal-main.py
+++
@@ -1,9 +0,0 @@
-def mostrar_menu_principal():
-    print("\n==================================")
-    print("=== PROYECTO BASE DE DATOS ===")
-    print("==================================")
-    print("1. Iniciar Sesion (Estudiante/Profesor/Admin)")
-    print("2. Registrar nuevo estudiante")
-    print("3. Salir")
-    opcion = input("Selecciona una opcion: ").strip()
-    return opcion
