678e99b30c2bef1552ca48c98e88f00e507eb625
nico-7483 <nico.riquelme009@gmail.com>
2025-11-04 00:17:09 -0300

    Delete __init__-controlador_estudiante.py

--- __init__-controlador_estudiante.py
+++
@@ -1,7 +0,0 @@
-def __init__(self, user_id=None):
-        self.vista = VistaEstudiante()
-        self.modelo_usuario = ModeloUsuario()
-        self.modelo_profesor = ModeloProfesor()
-        self.modelo_votacion = ModeloVotacion()
-        self.user_id = user_id 
-        self.rol = 'Estudiante'
