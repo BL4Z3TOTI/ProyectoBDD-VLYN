5f36a7471ce6fe68e2f6a48f9a3bb74439ae03ed
carloscarden <cardencarlos90@gmail.com>
2025-11-12 23:32:46 -0300

    prueba docker y modulo mediapipe de gesto pulgar

---
+++ Proyecto-Base-de-Datos/app/prueba.py
@@ -0,0 +1,28 @@
+import cv2
+
+
+print("cargo video")
+# Open the default camera
+cam = cv2.VideoCapture(0)
+
+
+
+while True:
+    ret, frame = cam.read()
+
+    if not ret:
+        print("OCURRIO ERROR!!!!!!")
+        break
+   
+    # Display the captured frame
+    cv2.imshow('Camera', frame)
+
+    # Press 'q' to exit the loop
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+# Release the capture and writer objects
+cam.release()
+out.release()
+cv2.destroyAllWindows()
+
